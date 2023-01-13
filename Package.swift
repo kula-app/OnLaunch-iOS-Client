6ad53d5712fe30a1920abc219112b002a9dd0bf4
Philip Niedertscheider <dev@philpri.me>
2023-01-13 14:31:01 +0100

    WIP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "OnLaunch", targets: ["OnLaunch"])
     ],
     targets: [
-        .target(name: "OnLaunch")
+        .target(name: "OnLaunch"),
         //dev .testTarget(name: "OnLaunchTests", dependencies: ["OnLaunch"]),
     ]
 )
