073ffc4b5dd78becedaac77e9274352c28ead687
Philip Niedertscheider <dev@philpri.me>
2023-01-13 10:32:32 +0100

    feat: added codecov step

--- Package.swift
+++ Package.swift
@@ -4,11 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "OnLaunch-iOS-Client",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         .library(name: "OnLaunch-iOS-Client", targets: ["OnLaunch-iOS-Client"]),
     ],
     targets: [
         .target(name: "OnLaunch-iOS-Client"),
-        .testTarget(name: "OnLaunch-iOS-ClientTests", dependencies: ["OnLaunch-iOS-Client"]),
+        //dev .testTarget(name: "OnLaunch-iOS-ClientTests", dependencies: ["OnLaunch-iOS-Client"]),
     ]
 )
