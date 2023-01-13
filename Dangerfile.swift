4eeaebbb3f4d6a9ab6bc175312758bb3c8924de7
Philip Niedertscheider <dev@philpri.me>
2023-01-13 14:00:38 +0100

    WIP

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -9,9 +9,9 @@ let sourceChanges = allSourceFiles.contains { $0.hasPrefix("Source") }
 let isNotTrivial = !danger.github.pullRequest.title.contains("#trivial")
 if isNotTrivial && sourceChanges {
     danger.warn("""
-         Any changes to library code should be reflected in the Changelog.
-         Please consider adding a note there.
-        """)
+     Any changes to library code should be reflected in the Changelog.
+     Please consider adding a note there.
+    """)
 }
 
 // Ensure no copyright header
@@ -19,18 +19,21 @@ let swiftFilesWithCopyright = allSourceFiles.filter {
     $0.contains("Copyright") && ($0.fileType == .swift  || $0.fileType == .m)
 }
 for file in swiftFilesWithCopyright {
-    fail(message: "Please remove this copyright header", file: file, line: 0)
+    danger.fail(message: "Please remove this copyright header", file: file, line: 0)
 }
 
 // Make it more obvious that a PR is a work in progress and shouldn't be merged yet
 if danger.github.pullRequest.title.contains("WIP") || danger.github.pullRequest.title.contains("Draft") {
-    warn("PR is classed as Work in Progress")
+    danger.warn("PR is classed as Work in Progress")
 }
 
 // Warn when there is a big PR
 var bigPRThreshold = 600;
-if ((danger.github.pullRequest.additions ?? 0) + (danger.github.pullRequest.deletions ?? 0) > bigPRThreshold) {
-  warn("Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split each into separate PR will helps faster, easier review.");
+if (danger.github.pullRequest.additions ?? 0) + (danger.github.pullRequest.deletions ?? 0) > bigPRThreshold {
+    danger.warn("""
+     Pull Request size seems relatively large. If this Pull Request contains multiple changes, please split
+     each into separate PR will helps faster, easier review.
+    """);
 }
 
 // Warning message for not updated package manifest(s)
@@ -49,15 +52,20 @@ if !updatedManifests.isEmpty && updatedManifests.count != manifests.count {
     let updatedVerb = updatedManifests.count == 1 ? "was" : "were"
     let notUpdatedArticle = notUpdatedManifests.count == 1 ? "the " : ""
 
-    warn("\(updatedArticle)\(updatedManifests.joined(separator: ", ")) \(updatedVerb) updated, " +
-         "but there were no changes in \(notUpdatedArticle)\(notUpdatedManifests.joined(separator: ", ")).\n" +
-         "Did you forget to update them?")
+    danger.warn("""
+     \(updatedArticle)\(updatedManifests.joined(separator: ", ")) \(updatedVerb) updated,
+     but there were no changes in \(notUpdatedArticle)\(notUpdatedManifests.joined(separator: ", ")).\n
+     Did you forget to update them?
+    """)
 }
 
 // Warn when library files has been updated but not tests.
 let testsUpdated = danger.git.modifiedFiles.contains { $0.hasPrefix("Tests") }
 if sourceChanges && !testsUpdated {
-    warn("The library files were changed, but the tests remained unmodified. Consider updating or adding to the tests to match the library changes.")
+    warn("""
+     The library files were changed, but the tests remained unmodified.
+     Consider updating or adding to the tests to match the library changes.
+    """)
 }
 
 // Run Swiftlint
