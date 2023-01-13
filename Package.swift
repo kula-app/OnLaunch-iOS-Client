651710bceeafffc97a8934c3952a9d4f26538b58
Philip Niedertscheider <phil@kula.app>
2023-01-13 11:49:11 +0100

    feat: added GitHub actions (#5)

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "OnLaunch-iOS-Client",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         .library(name: "OnLaunch-iOS-Client", targets: ["OnLaunch-iOS-Client"]),
     ],
     targets: [
         .target(name: "OnLaunch-iOS-Client"),
-        .testTarget(name: "OnLaunch-iOS-ClientTests", dependencies: ["OnLaunch-iOS-Client"]),
+        //dev .testTarget(name: "OnLaunch-iOS-ClientTests", dependencies: ["OnLaunch-iOS-Client"]),
     ]
 )
